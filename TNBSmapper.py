4f739673d612cd491bf5f2ddd1e0f48d00e09aff
Srdjan Sumarac <sumaracsrdjan@gmail.com>
2022-01-19 20:26:40 -0500

    annotations

--- TNBSmapper.py
+++ TNBSmapper.py
@@ -401,7 +401,7 @@ if invert == "Normal":
     ax.invert_xaxis()
 
 labels = "Patient ID:"+str(patient_id)+"\n"+"MRN:"+str(mrn)+"\n"+"Sex: "+str(sex)+"\n"+"DOB: "+str(dob)+"\n"+"OP Date: "+str(op_date)+"\n"+"Surgeon: "+str(surgeon)+"\n"+"Target: "+str(target)+"\n"+"Track: "+str(s_track)+"\n"+"Hemisphere: "+str(hemi)+"\n"+"AC(x,y,z): "+"("+str(AC_X)+","+str(AC_Y)+","+str(AC_Z)+")""\n"+"PC(x,y,z): "+"("+str(PC_X)+","+str(PC_Y)+","+str(PC_Z)+")""\n"+"Coord(x,y,z): "+"("+str(X)+","+str(Y)+","+str(Z)+")""\n"+"Ring Angle: "+str(Ring)+"°"+"\n"+"Arc Angle: "+str(Arc)+"°"+"\n"
-anchored_text1 = AnchoredText(labels, prop=dict(size=6),loc=1)
+anchored_text1 = AnchoredText(labels, prop=dict(size=6,color='black'),loc=1)
 ax.add_artist(anchored_text1)
 
 st.write(fig)
