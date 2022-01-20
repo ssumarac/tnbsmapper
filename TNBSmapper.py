97d5b1a904964434da99c835609938d8775c4b2c
Srdjan Sumarac <sumaracsrdjan@gmail.com>
2022-01-19 21:29:54 -0500

    final

--- TNBSmapper.py
+++ TNBSmapper.py
@@ -19,7 +19,7 @@ st.sidebar.subheader("Patient Information")
 patient_id = st.sidebar.number_input("Patient ID (MER)",step=1)
 mrn = st.sidebar.number_input("MRN",step=1)
 sex = st.sidebar.selectbox("Sex",["","Male","Female"])
-dob = st.sidebar.date_input("DOB",value=(datetime(1800, 1, 1)))
+dob = st.sidebar.date_input("DOB",value=(datetime(1900, 1, 1)),max_value=150)
 
 st.sidebar.subheader("Surgery Information")
 op_date = st.sidebar.date_input("Operation Date")
