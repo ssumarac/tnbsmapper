facc7d1053d05ae0c90a01bf9abcd8f70cd53220
ssumarac <sumaracsrdjan@gmail.com>
2022-02-07 10:37:58 -0500

    fix

--- TNBSmapper.py
+++ TNBSmapper.py
@@ -18,7 +18,7 @@ st.image(Image.open('tnbs_logo.png'),width=150)
 st.sidebar.subheader("Patient Information")
 patient_name = st.sidebar.text_input("Patient Name")
 patient_id = st.sidebar.text_input("Patient ID (MER)")
-mrn = st.sidebar.text_input("MRN",step=1)
+mrn = st.sidebar.text_input("MRN")
 sex = st.sidebar.selectbox("Sex",["","Male","Female"])
 dob = st.sidebar.date_input("DOB",value=(datetime(1900, 1, 1)),min_value=(datetime(1900, 1, 1)), max_value=(datetime(2030, 1, 1)))
 
